8824620c305ab61723690685a5978fdabf15ff20
Borsatole <borsatole@gmail.com>
2024-01-29 17:30:03 -0300

    responsividade/atualizações

--- script/app.js
+++ script/app.js
@@ -1,5 +1,32 @@
+// Menu Mobile Toggle: Abrir / Fechar
+
+let btn_menu_abrir = document.querySelector('.open');
+let btn_menu_fechar = document.querySelector('.close');
+let lista_menu = document.querySelector('.menu_mobile_lista');
+
+
+function menu_toggle_abrir () {
+    btn_menu_abrir.style.display = "none";
+    btn_menu_fechar.style.display = "flex";
+    lista_menu.style.display = "flex";
+
+}
+
+function menu_toggle_fechar () {
+    btn_menu_abrir.style.display = "flex";
+    btn_menu_fechar.style.display = "none";
+    lista_menu.style.display = "none";
+}
+
+// Fim das funções do menu mobile
+
+// Botão: Cadastros > Adicionar usuario
 let btn_adicionar_usuario = document.getElementsByClassName('btn_add');
+
+// Formulario: Cadastros > Adicionar usuario
 let modal_cadastro_de_usuario = document.querySelector('.formulariobg');
+
+// Formulario: Cadastros > X Fechar
 let btn_fechar_modal = document.querySelector('.btn_fechar_formulario');
 
 
@@ -12,3 +39,7 @@ function fechar_modal() {
     modal_cadastro_de_usuario.style.display = "none";
     console.log("botão fechar modal - foi clicado");
 }
+
+
+
+
